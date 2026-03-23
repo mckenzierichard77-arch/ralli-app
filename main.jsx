c9c206de271c8a87bd1428737f68e413bdba0a18
mckenzierichard77-arch <mckenzierichard77@gmail.com>
2026-03-23 15:42:06 -0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../RalliGoodSisters_v8.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
