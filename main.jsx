f1337768fc920e05e2eb3fbd633e5728d112ef1f
mckenzierichard77-arch <mckenzierichard77@gmail.com>
2026-03-23 16:00:54 -0700

    Delete main.jsx

--- main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './RalliGoodSisters_v8 (18).jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
