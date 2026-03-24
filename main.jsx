210dba0d229e51f3f723520267cce6bbfe178ec3
mckenzierichard77-arch <mckenzierichard77@gmail.com>
2026-03-23 17:27:55 -0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '../RalliGoodSisters_v8.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+import App from './RalliGoodSisters_v8.jsx'
