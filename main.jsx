63948bc2076633bcd2400c2116c1bb6e5bf8b109
mckenzierichard77-arch <mckenzierichard77@gmail.com>
2026-03-23 18:20:50 -0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,7 @@
-import App from './RalliGoodSisters_v8.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RalliApp from './RalliGoodSisters_v8.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RalliApp />
+)
